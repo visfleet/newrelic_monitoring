e9ac2a61fbca11b7fac49ea24e7ddc61d6ac7d95
Stephen <ssalinas@users.noreply.github.com>
2014-04-01 13:34:50 -0400

    Faster nokogiri bundle install

--- definitions/bundle_install.rb
+++ definitions/bundle_install.rb
@@ -7,7 +7,7 @@ define :bundle_install do
   # bundle install
   execute 'bundle install' do
     cwd params[:path]
-    command "bundle install"
+    command "env NOKOGIRI_USE_SYSTEM_LIBRARIES=true bundle install"
     user params[:user]
     only_if { File.directory?(params[:path]) }
   end
